e9da6ae6aa7306e26c7ec5c28638ae48e3866b80
ritiksharmarj <bloggeritik@gmail.com>
2022-11-27 02:52:22 +0530

    Build a screen

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-export default function App() {
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+
+const placeHolderImage = require('./assets/images/background-image.png');
+
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>My First React Native App - Twinte</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeHolderImageSource={placeHolderImage} />
+      </View>
+
+      <View style={styles.footerContainer}>
+        <Button theme='Primary' label='Choose a photo' />
+        <Button label='Use this photo' />
+      </View>
       <StatusBar style='auto' />
     </View>
   );
-}
+};
 
+// React Native Styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
 });
+
+export default App;
